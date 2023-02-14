9ff6af2f402c664aa71869b14afdf937f56d43a5
Brian Xu <mattx0121@gmail.com>
2023-02-14 12:01:26 -0800

    change commit

--- CloudImporter.ts
+++ CloudImporter.ts
@@ -2,24 +2,28 @@ import { Playlist } from "./Playlist";
 import { Album } from "./Album";
 import { Artist } from "./Artist";
 
-/*class CloudImporter implements code here {
-      private _url: string;
+interface IImportable {
+      loadPlaylist(): Playlist;
+    }
+    
+/*class CloudImporter implements IImportable {
+      private url: string;
     
       constructor(url: string) {
-        this._url = url;
+        this.url = url;
       }
-}*/
+    
+     public loadPlaylist(): Playlist { some error here
+        console.log(`Importing playlist from ${this.url}`); 
+    
+        // Create and return a fake playlist 
+        //const playlist = new Playlist("My Playlist");
+        const album = new Album("The 2nd Law", artist, 2012);  some error here
+        album.addTrack(new Song("Fake Song 1"));
+        album.addTrack(new Song("Fake Song 2"));
+        playlist.addAlbum(album);
+        return playlist;
+      }
+     */ 
 
-// public loadPlaylist() :Playlist { can not fix the return a value error
-// console.log(`Importing playlist from ${this._url}`)
-
-//create and return a fake playlist
-//const playlist = new Playlist("My Playlist")
-// const album = new Album("the 2nd Law",artist, 2012); can not fix the problem here, so command all the code below. 
-// album.addTrack(new Song(" Song 1"));
-// album.addTrack(new Song(" Song 2"));
-// playlist.addAlbum(album);
-// return playlist;
- 
-
-
+    
